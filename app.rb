3267b5fbe51269409cb36a1f82e3ef3e84bb9dd5
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-24 16:29:39 +0100

    can view listings

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require_relative './lib/listing'
 
 class App < Sinatra::Base
   get '/' do
     'Hello App!'
+  end
+
+  get '/listings' do
+    @listings = Listing.all
+    erb :'listings/index'
   end
 
   # start the server if ruby file executed directly
