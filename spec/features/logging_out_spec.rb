ee34c1ab0b602d031e5fe449be7d0d9cbda1bcd7
Charlie Slater <charliemslater1701@gmail.com>
2021-05-25 15:18:24 +0100

    refactor logging out test

--- spec/features/logging_out_spec.rb
+++ spec/features/logging_out_spec.rb
@@ -1,11 +1,7 @@
 feature 'logging a user out' do
   # need to test for invalid email / passwords
   scenario 'it clears a user from the session' do
-    visit('/users/new')
-    fill_in(:email, with: 'email@gmail.com')
-    fill_in(:password, with: 'password')
-    click_button('Submit')
-
+    register_new_user
     click_button('Sign out')
 
     expect(page).not_to have_content('Hello, email@gmail.com')
