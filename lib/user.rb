9ce304e4dbc7e36b1018a3da2636f83feca11ed7
Charlie Slater <charliemslater1701@gmail.com>
2021-05-25 15:47:04 +0100

    changed to use exec_params

--- lib/user.rb
+++ lib/user.rb
@@ -6,8 +6,8 @@ class User
     dbname = ENV['RACK_ENV'] == 'test' ? 'makersbnb_test' : 'makersbnb'
     connection = PG.connect(dbname: dbname)
     encrypted_password = BCrypt::Password.create(password)
-    result = connection.exec("INSERT INTO users (email, password) VALUES('#{email}', '#{encrypted_password}')
-    RETURNING id, email")
+    result = connection.exec_params("INSERT INTO users (email, password) VALUES($1, $2)
+    RETURNING id, email", [email, encrypted_password])
     User.new(result.first['id'], result.first['email'])
   end
 
@@ -21,7 +21,7 @@ class User
   def self.login(email, password)
     dbname = ENV['RACK_ENV'] == 'test' ? 'makersbnb_test' : 'makersbnb'
     connection = PG.connect(dbname: dbname)
-    user = connection.exec("SELECT * FROM users WHERE email = '#{email}'")
+    user = connection.exec_params("SELECT * FROM users WHERE email = $1", [email])
     return unless user.any?
     BCrypt::Password.new(user.first['password']) == password ? User.new(user.first['id'], user.first['email']) : nil  
   end
