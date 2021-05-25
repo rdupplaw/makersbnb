115f55c629e8b3419591b9e15acd4eb7516f22ce
Charlie Slater <charliemslater1701@gmail.com>
2021-05-25 11:33:57 +0100

    added test env to user model

--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,15 @@
 class User
   def self.register(email:, password:)
-    connection = PG.connect(dbname: 'makersbnb_test')
+    dbname = ENV['RACK_ENV'] == 'test' ? 'makersbnb_test' : 'makersbnb'
+    connection = PG.connect(dbname: dbname)
     result = connection.exec("INSERT INTO users (email, password) VALUES('#{email}', '#{password}')
     RETURNING id, email")
     User.new(result.first['id'], result.first['email'])
   end
 
   def self.find(user_id)
-    connection = PG.connect(dbname: 'makersbnb_test')
+    dbname = ENV['RACK_ENV'] == 'test' ? 'makersbnb_test' : 'makersbnb'
+    connection = PG.connect(dbname: dbname)
     result = connection.exec("SELECT * FROM users WHERE id = '#{user_id}'")
     User.new(result.first['id'], result.first['email'])
   end
