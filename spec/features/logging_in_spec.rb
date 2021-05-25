57611683e8875ade04da3d16cd4b6202b5dc13ca
Charlie Slater <charliemslater1701@gmail.com>
2021-05-25 15:52:43 +0100

    uncomment

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -1,15 +1,15 @@
-# feature 'logging in' do
-#   scenario 'user is logged in' do
-#     register_new_user
+feature 'logging in' do
+  scenario 'user is logged in' do
+    register_new_user
 
-#     click_button('Sign out')
-#     click_button('Sign in')
+    click_button('Sign out')
+    click_button('Sign in')
 
-#     fill_in(:email, with: 'email@gmail.com')
-#     fill_in(:password, with: 'password')
-#     click_button('Submit')
+    fill_in(:email, with: 'email@gmail.com')
+    fill_in(:password, with: 'password')
+    click_button('Submit')
 
-#     expect(current_path).to eq('/listings')
-#     expect(page).to have_content('Hello, email@gmail.com')
-#   end
-# end
+    expect(current_path).to eq('/listings')
+    expect(page).to have_content('Hello, email@gmail.com')
+  end
+end
