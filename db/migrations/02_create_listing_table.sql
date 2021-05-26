30c63639a1f482e8a8700356390362472861a2e1
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-26 15:06:01 +0100

    reorderd table creation

--- db/migrations/02_create_listing_table.sql
+++ db/migrations/02_create_listing_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE listings (id SERIAL PRIMARY KEY,
                                          name VARCHAR(30),
                                               description VARCHAR(240),
-                                                          price MONEY);
+                                                          price MONEY,
+                                                          owner_id INTEGER REFERENCES users(id));
